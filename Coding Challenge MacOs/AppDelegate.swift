4e546fdc329022aeee11d90c0e892e8ae380e0d8
98kmc <centenokm.ing@gmail.com>
2024-01-31 17:12:01 -0600

    Initial Commit

---
+++ Coding Challenge MacOs/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Coding Challenge MacOs
+//
+//  Created by Miguel on 31/01/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
